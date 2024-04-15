269b04658c613d1f46929c114409725303e40679
Anderson <beltrananderson283@gmail.com>
2024-04-15 15:23:42 -0500

    Se ha modificado el menu

--- frontend/src/componentes/Cronograma/Cronograma.jsx
+++ frontend/src/componentes/Cronograma/Cronograma.jsx
@@ -1,10 +1,9 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import "./css/Cronograma.css";
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
 import 'moment/locale/es';
-
 
 const localizer = momentLocalizer(moment);
 
@@ -24,11 +23,25 @@ const messages = {
     localizer.format(date, 'dddd', culture),
 };
 
-
-
 function Cronograma() {
   moment.locale('es');
   const [events, setEvents] = useState([]);
+
+  // Función para ocultar los botones de navegación específicos
+  // const hideNavigationButtons = () => {
+  //   const navigationButtons = document.querySelectorAll('.rbc-toolbar button');
+  //   navigationButtons.forEach(button => {
+  //     const buttonText = button.textContent.toLowerCase();
+  //     if (['mes', 'semana', 'día', 'agenda'].includes(buttonText)) {
+  //       button.style.display = 'none';
+  //     }
+  //   });
+  // };
+
+  // // Ocultar los botones de navegación al montar el componente
+  // useEffect(() => {
+  //   hideNavigationButtons();
+  // }, []);
 
   return (
     <div className="Cronograma-box">
--- frontend/src/componentes/layouts/Sidenav/Sidenav.jsx
+++ frontend/src/componentes/layouts/Sidenav/Sidenav.jsx
@@ -1,47 +1,60 @@
-import React, { Fragment } from 'react';
-import './css/Sidenav.css';
-import LogoTeinnovaHome from './img/Teinnova-logo-menu.png';
+import React, { Fragment } from "react";
+import "./css/Sidenav.css";
+import LogoTeinnovaHome from "./img/Teinnova-logo-menu.png";
 import { FaCalendarCheck } from "react-icons/fa";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import { GiNotebook } from "react-icons/gi";
 import { ImUsers } from "react-icons/im";
 
 function Sidenav() {
   return (
     <Fragment>
-        <div className='Sidenav-content'>
-            <ul className='list-group Sidenav-content__menu-content'>
-                <li>
-                    <div className='Sidenav-content__menu-content__Logo-Teinnova'>
-                        <img src={LogoTeinnovaHome} alt='Logo Teinnova'/>
-                    </div>
-                </li>
-                <li className="Sidenav-content__menu-content__menu-options">
-                        <Link to={"/actividades"}><FaCalendarCheck className='inline-block mr-2'/> 
-                        Actividades</Link>
-                    </li>
-                    <li className="Sidenav-content__menu-content__menu-options">
-                        <GiNotebook className='inline-block'/> Cronograma
-                    </li>
-                    <li className="Sidenav-content__menu-content__menu-options">
-                        Eventos
-                    </li>
-                    <li className="Sidenav-content__menu-content__menu-options">
-                        Programa
-                    </li>
-                    <li className="Sidenav-content__menu-content__menu-options">
-                        Proyectos
-                    </li>
-                    <li className="Sidenav-content__menu-content__menu-options">
-                        Semillero
-                    </li>
-                    <li className="Sidenav-content__menu-content__menu-options">
-                        <ImUsers className='inline-block'/> Usuarios
-                    </li>
-            </ul>
-        </div>
+      <div className="Sidenav-content">
+        <ul className="list-group Sidenav-content__menu-content">
+          <li>
+            <div className="Sidenav-content__menu-content__Logo-Teinnova">
+              <img src={LogoTeinnovaHome} alt="Logo Teinnova" />
+            </div>
+          </li>
+          <Link to={"/actividades"}>
+            <li className="Sidenav-content__menu-content__menu-options">
+              <FaCalendarCheck className="inline-block mr-2" /> Actividades
+            </li>
+          </Link>
+          <Link to={"/cronograma"}>
+            <li className="Sidenav-content__menu-content__menu-options">
+              <GiNotebook className="inline-block" /> Cronograma
+            </li>
+          </Link>
+          <Link>
+            <li className="Sidenav-content__menu-content__menu-options">
+              Eventos
+            </li>
+          </Link>
+          <Link>
+            <li className="Sidenav-content__menu-content__menu-options">
+              Programa
+            </li>
+          </Link>
+          <Link>
+            <li className="Sidenav-content__menu-content__menu-options">
+              Proyectos
+            </li>
+          </Link>
+          <Link>
+            <li className="Sidenav-content__menu-content__menu-options">
+              Semillero
+            </li>
+          </Link>
+          <Link>
+            <li className="Sidenav-content__menu-content__menu-options">
+              <ImUsers className="inline-block" /> Usuarios
+            </li>
+          </Link>
+        </ul>
+      </div>
     </Fragment>
-  )
+  );
 }
 
 export default Sidenav;
