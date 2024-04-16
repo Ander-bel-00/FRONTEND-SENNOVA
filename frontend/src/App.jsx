40aa8b3736e8ee45c548e5387b622707a1e96f82
Anderson <beltrananderson283@gmail.com>
2024-04-16 18:22:55 -0500

    Cambios en el App.jsx, menu y header y rutas absolutas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import Visualizar_Actividad from './Componentes/visualizar_actividad/Visualizar_Actividad';
-import Listar_Eventos from './Componentes/listar_eventos/Listar_Eventos';
-import Visualizar_Evento from './Componentes/visualizar_evento/Visualizar_Evento';
-import Visualizar_Programa_Formacion from './Componentes/visualizar_programa_formacion/Visualizar_Programa_Formacion';
-import Crear_Programa_Formacion from './Componentes/crear_programa_formacion/Crear_Programa_Formacion';
-import Crear_Eventos from './Componentes/crear_eventos/Crear_Eventos';
+import Visualizar_Actividad from './componentes/visualizar_actividad/Visualizar_Actividad';
+import Listar_Eventos from './componentes/listar_eventos/Listar_Eventos';
+import Visualizar_Evento from './componentes/visualizar_evento/Visualizar_Evento';
+import Visualizar_Programa_Formacion from './componentes/visualizar_programa_formacion/Visualizar_Programa_Formacion';
+import Crear_Programa_Formacion from './componentes/crear_programa_formacion/Crear_Programa_Formacion';
+import Crear_Eventos from './componentes/crear_eventos/Crear_Eventos';
+import { Fragment } from 'react';
+import Header from './componentes/layouts/Header/Header';
+import Sidenav from './componentes/layouts/Sidenav/Sidenav';
 
 
 function App () {
@@ -12,49 +15,58 @@ function App () {
     <> 
       <BrowserRouter>
         <Routes>
+          <Route 
+            path='/lider-semillero/*'
+            element={
+              <Fragment>
+                <Header />
+                <Sidenav />
+                <Routes>
+                <Route 
+                  path='/Visualizar-actividad'
+                  element={
+                    <Visualizar_Actividad />
+                  }
+                />
 
-          <Route 
-            path='/Visualizar-actividad'
-            element={
-              <Visualizar_Actividad />
-            }
-          />
+                <Route 
+                  path='/Listar-eventos'
+                  element={
+                    <Listar_Eventos />
+                  }
+                />
 
-          <Route 
-            path='/Listar-eventos'
-            element={
-              <Listar_Eventos />
-            }
-          />
+                <Route 
+                  path='/Visualizar-evento'
+                  element={
+                    <Visualizar_Evento/>
+                  }
+                
+                />
+                <Route 
+                  path='/Visualizar-programa-formacion'
+                  element={
+                    <Visualizar_Programa_Formacion/>
+                  }
+                
+                />
+                <Route 
+                  path='/Crear-programa-formacion'
+                  element={
+                    <Crear_Programa_Formacion/>
+                  }
+                />
 
-          <Route 
-            path='/Visualizar-evento'
-            element={
-              <Visualizar_Evento/>
-            }
-          
-          />
-          <Route 
-            path='/Visualizar-programa-formacion'
-            element={
-              <Visualizar_Programa_Formacion/>
-            }
-          
-          />
-          <Route 
-            path='/Crear-programa-formacion'
-            element={
-              <Crear_Programa_Formacion/>
+                <Route 
+                  path='/Crear-eventos'
+                  element={
+                    <Crear_Eventos/>
+                  }
+                />
+                </Routes>
+              </Fragment>
             }
           />
-
-          <Route 
-            path='/Crear-eventos'
-            element={
-              <Crear_Eventos/>
-            }
-          />
-          
         </Routes>
       </BrowserRouter>
     </>
