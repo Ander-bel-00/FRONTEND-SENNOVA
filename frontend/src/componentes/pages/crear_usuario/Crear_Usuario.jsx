89e7b4a0267e5f404b51f930d5a20f10a1bfaeac
Anderson <beltrananderson283@gmail.com>
2024-04-27 09:07:37 -0500

    Empezando a integrar con backend, implementando logica para crear usuarios

--- frontend/src/componentes/pages/crear_usuario/Crear_Usuario.jsx
+++ frontend/src/componentes/pages/crear_usuario/Crear_Usuario.jsx
@@ -4,8 +4,46 @@ import { Link } from "react-router-dom";
 import Caja_formularios from "../../common/Caja_formularios";
 import { GiReturnArrow } from "react-icons/gi";
 import BotonReturn from "../../common/BotonReturn";
+import clienteAxios from "../../../config/axios";
+import Swal from "sweetalert2";
 
 function Crear_Usuario() {
+  const [formDataUser, SetFormDataUser] = useState({});
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    SetFormDataUser({ ...formDataUser, [name]: value });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await clienteAxios.post("/create-user/", formDataUser);
+      Swal.fire({
+        title: "Usuario registrado exitosamente",
+        icon: "success",
+        showCancelButton: false,
+        confirmButtonText: "Aceptar",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          window.location.href = "/";
+        }
+      });
+
+      console.log(response.data);
+    } catch (error) {
+      console.error("Error al crear el Usuario:", error);
+
+      // Mostrar Mensaje e caso de error al crear el Usuario.
+      Swal.fire({
+        title: "Error",
+        text: "Hubo un error al crear el Usuario",
+        icon: "error",
+        confirmButtonText: "Aceptar",
+      });
+    }
+  };
+
   // Función para declarar un estado que almacenará el rol seleccionado
   const [rolUsuario, setRolUsuario] = useState("instructor investigador");
   // Función para mostrar campos adicionales en el formulario solo si se selecciona aprendiz investigador
@@ -45,107 +83,138 @@ function Crear_Usuario() {
                   </option>
                 </select>
               </div>
-              <form className="form-add-user-container">
-                <label
+              <form className="form-add-user-container" onSubmit={handleSubmit}>
+                {/* <label
                   htmlFor="nombre-proyecto"
                   className="form-add-user-container__col1__label"
                 >
                   Tipo de Documento <p className="rojo-required">*</p>
                 </label>
-                <input
+                <select
                   type="text"
-                  id="nombre-proyecto"
+                  id="tipo-documento"
                   className="form-add-user-container__col1__input"
-                />
-                <label
-                  htmlFor="nombre-proyecto"
-                  className="form-add-user-container__col1__label"
+                  onChange={handleChange}
                 >
-                  Nombres <p className="rojo-required">*</p>
-                </label>
-                <input
-                  type="text"
-                  id="nombre-proyecto"
-                  className="form-add-user-container__col1__input"
-                />
+                  <option value="">Cédula de Ciudadania</option>
+                  <option value="">Tarjeta de Identidad</option>
+                  <option value="">Cédula de Extranjeria</option>
+                </select> */}
                 <label
-                  htmlFor="nombre-proyecto"
-                  className="form-add-user-container__col1__label"
-                >
-                  Estado de Usuario <p className="rojo-required">*</p>
-                </label>
-                <input
-                  type="text"
-                  id="nombre-proyecto"
-                  className="form-add-user-container__col1__input"
-                />
-
-                <label
-                  htmlFor="nombre-proyecto"
+                  htmlFor="documento"
                   className="form-add-user-container__col1__label"
                 >
                   Número de Documento <p className="rojo-required">*</p>
                 </label>
                 <input
                   type="text"
-                  id="nombre-proyecto"
+                  name="documento"
+                  id="documento"
                   className="form-add-user-container__col1__input"
+                  onChange={handleChange}
                 />
 
                 <label
-                  htmlFor="nombre-proyecto"
+                  htmlFor="name"
+                  className="form-add-user-container__col1__label"
+                >
+                  Nombres <p className="rojo-required">*</p>
+                </label>
+                <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  className="form-add-user-container__col1__input"
+                  onChange={handleChange}
+                />
+                <label
+                  htmlFor="last_names"
                   className="form-add-user-container__col1__label"
                 >
                   Apellidos <p className="rojo-required">*</p>
                 </label>
                 <input
                   type="text"
-                  id="nombre-proyecto"
+                  name="last_names"
+                  id="last_names"
                   className="form-add-user-container__col1__input"
+                  onChange={handleChange}
                 />
                 <label
-                  htmlFor="nombre-proyecto"
+                  htmlFor="telefono"
+                  className="form-add-user-container__col1__label"
+                >
+                  Teléfono <p className="rojo-required">*</p>
+                </label>
+                <input
+                  type="number"
+                  name="telefono"
+                  id="telefono"
+                  className="form-add-user-container__col1__input"
+                  onChange={handleChange}
+                />
+                <label
+                  htmlFor="last_names"
+                  className="form-add-user-container__col1__label"
+                >
+                  Email <p className="rojo-required">*</p>
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  className="form-add-user-container__col1__input"
+                  onChange={handleChange}
+                />
+                <label
+                  htmlFor="rol"
                   className="form-add-user-container__col1__label"
                 >
                   Rol de Usuario <p className="rojo-required">*</p>
                 </label>
                 <input
                   type="text"
-                  id="nombre-proyecto"
+                  id="rol"
+                  name="rol"
                   className="form-add-user-container__col1__input"
-                  value={"instructor investigador"}
+                  value={rolUsuario}
                   readOnly
+                  onChange={handleChange}
                 />
                 {/* Campos adicionales solo si se selecciona el rol de "aprendiz investigador" */}
 
                 {mostrarCamposAprendiz && (
                   <Fragment>
                     <label
-                      htmlFor="campo-adicional-1"
-                      className="form-add-user-container__col1__label"
-                    >
-                      Programa de Formación <p className="rojo-required">*</p>
-                    </label>
-                    <input
-                      type="text"
-                      id="campo-adicional-1"
-                      className="form-add-user-container__col1__input"
-                    />
-
-                    <label
-                      htmlFor="campo-adicional-1"
+                      htmlFor="ficha"
                       className="form-add-user-container__col1__label"
                     >
                       Número de Ficha <p className="rojo-required">*</p>
                     </label>
                     <input
                       type="text"
-                      id="campo-adicional-1"
+                      id="ficha"
+                      name="ficha"
                       className="form-add-user-container__col1__input"
+                      onChange={handleChange}
                     />
 
                     <label
-                      htmlFor="campo-adicional-1"
+                      htmlFor="programa_formacion"
+                      className="form-add-user-container__col1__label"
+                    >
+                      Programa de Formación <p className="rojo-required">*</p>
+                    </label>
+                    <input
+                      type="text"
+                      id="programa_formacion"
+                      name="programa_formacion"
+                      className="form-add-user-container__col1__input"
+                      onChange={handleChange}
+                    />
+
+                    <label
+                      htmlFor="inicio_lectiva"
                       className="form-add-user-container__col1__label"
                     >
                       Incio Lectiva De La Ficha{" "}
@@ -153,25 +222,29 @@ function Crear_Usuario() {
                     </label>
                     <input
                       type="date"
-                      id="campo-adicional-1"
+                      id="inicio_lectiva"
+                      name="inicio_lectiva"
                       className="form-add-user-container__col1__input"
+                      onChange={handleChange}
                     />
 
                     <label
-                      htmlFor="campo-adicional-1"
+                      htmlFor="finalizacion_lectiva"
                       className="form-add-user-container__col1__label"
                     >
                       Fin Lectiva De La Ficha <p className="rojo-required">*</p>
                     </label>
                     <input
                       type="date"
-                      id="campo-adicional-1"
+                      id="finalizacion_lectiva"
+                      name="finalizacion_lectiva"
                       className="form-add-user-container__col1__input"
+                      onChange={handleChange}
                     />
                   </Fragment>
                 )}
                 <div className="user-add-btns">
-                  <button type="button" className="btn-crear-usuario">
+                  <button type="submit" className="btn-crear-usuario">
                     Crear
                   </button>
 
