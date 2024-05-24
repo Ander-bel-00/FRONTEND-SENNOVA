86df77eb298ffd9738aef3dd3308445bbd7723ac
Karol <montoyakr26@gmail.com>
2024-05-24 13:58:41 -0500

    generar reporte en excel admin

--- frontend/src/componentes/pages/Admin/Visualizar_Programa_Formacion_Admin/Visualizar_Programa_Formacion_Admin.jsx
+++ frontend/src/componentes/pages/Admin/Visualizar_Programa_Formacion_Admin/Visualizar_Programa_Formacion_Admin.jsx
@@ -6,13 +6,17 @@ import BotonBlanco from "../../../common/BotonReporte";
 import BotonVerdeAñadir from "../../../common/BotonVerde";
 import './css/Visualizar_Programa_Formacion_Admin.css';
 import { Fragment } from "react";
+import * as XLSX from "xlsx";
 
 function Visualizar_Programa_Formacion_Admin() {
   const Visualizar = [
     {
       codigo: 2895665,
       version: "la másfresa",
-      nombre: "Mariposa"
+      nombre: "Mariposa",
+      ficha: "2895664",
+      inicio_electiva: "16/05/2024",
+      fin_electiva: "25/11/2014",
     },
     {
       codigo: 2895665,
@@ -21,12 +25,48 @@ function Visualizar_Programa_Formacion_Admin() {
     }
   ]
 
+  const exportToExcel = () => {
+    const wb = XLSX.utils.book_new();
+    const wsData = [
+      [
+        "Código del Programa",
+        "Versión del Programa",
+        "Nombre del Programa",
+        "Ficha",
+        "Inicio Electiva",
+        "Fin Electiva",
+      ],
+      ...Visualizar.map((Visualizar) => [
+        Visualizar.codigo,
+        Visualizar.version,
+        Visualizar.ficha,
+        Visualizar.inicio_electiva,
+        Visualizar.fin_electiva,
+      ]),
+    ];
+    const ws = XLSX.utils.aoa_to_sheet(wsData);
+
+    // Agrega estilos de tabla a la hoja de cálculo
+    ws["!cols"] = [
+      { width: 40 },
+      { width: 40 },
+      { width: 40 },
+      { width: 40 },
+      { width: 40 },
+      { width: 40 },
+    ];
+
+    // Genera el archivo Excel
+    XLSX.utils.book_append_sheet(wb, ws, "Programas_formacion");
+    XLSX.writeFile(wb, "proyectos.xlsx");
+  };
+
   return (
     <div className="main-container__contenedor-hijo">
       <Header_ToolBar
       Header_Tools={
         <Fragment>
-          <BotonBlanco icon={<FaFileArrowUp/>} text={"reporte"} clase={'btn-blanco btn-blanco--modify btn-verde'}/>
+          <BotonBlanco icon={<FaFileArrowUp/>} text={"reporte"} clase={'btn-blanco btn-blanco--modify btn-verde'} onClick={exportToExcel}/>
           <BotonVerdeAñadir icon={<IoAdd/>} text={"Crear Programa de Formación"} link={'/admin/Crear-programa-formacion'}/>
         </Fragment>
       }
@@ -37,9 +77,12 @@ function Visualizar_Programa_Formacion_Admin() {
           <table className="list-visualize-table-admin">
             <thead>
               <tr className="list-visualize-table__tr-admin">
-                <th className= "list-visualize-table__th-admin" >Código</th>
-                <th className= "list-visualize-table__th-admin" >Versión</th>
-                <th className= "list-visualize-table__th-admin" >Nombre</th>
+                <th className= "list-visualize-table__th-admin" >Código del Programa</th>
+                <th className= "list-visualize-table__th-admin" >Versión del Programa</th>
+                <th className= "list-visualize-table__th-admin" >Nombre del Programa</th>
+                <th className= "list-visualize-table__th-admin" >Ficha</th>
+                <th className= "list-visualize-table__th-admin" >Inicio Electiva</th>
+                <th className= "list-visualize-table__th-admin" >Fin Electiva</th>
               </tr>
             </thead>
 
@@ -49,6 +92,9 @@ function Visualizar_Programa_Formacion_Admin() {
                 <td className= "list-visualize-table__td-admin"  key={index}> {Visualizar.codigo} </td>
                 <td className= "list-visualize-table__td-admin" > {Visualizar.version} </td>
                 <td className= "list-visualize-table__td-admin" > {Visualizar.nombre} </td>
+                <td className= "list-visualize-table__td-admin" > {Visualizar.ficha} </td>
+                <td className= "list-visualize-table__td-admin" > {Visualizar.inicio_electiva} </td>
+                <td className= "list-visualize-table__td-admin" > {Visualizar.fin_electiva} </td>
               </tr>
               ))}
             </tbody>
--- frontend/src/componentes/pages/Admin/Visualizar_Usuario_Admin/Visualizar_Usuario_Admin.jsx
+++ frontend/src/componentes/pages/Admin/Visualizar_Usuario_Admin/Visualizar_Usuario_Admin.jsx
@@ -9,8 +9,57 @@ import Caja_Blanca from '../../../common/Caja_Blanca';
 import BotonReturn from '../../../common/BotonReturn';
 import { GiReturnArrow } from "react-icons/gi";
 import { Fragment } from "react";
+import * as XLSX from "xlsx";
 
 function Visualizar_Usuario_Admin() {
+  const Usuario = [
+    {
+      nombres: "Yesid",
+      apellidos: "Molina",
+      documento: "1059695065",
+      correo: "yesid@gmail.com",
+      telefono: "3192819219",
+      rol: "admin",
+    }
+  ]
+
+  const exportToExcel = () => {
+    const wb = XLSX.utils.book_new();
+    const wsData = [
+      [
+        "Código del Programa",
+        "Versión del Programa",
+        "Nombre del Programa",
+        "Ficha",
+        "Inicio Electiva",
+        "Fin Electiva",
+      ],
+      ...Usuario.map((Visualizaruser) => [
+        Visualizaruser.name,
+        Visualizaruser.last_names,
+        Visualizaruser.documento,
+        Visualizaruser.email,
+        Visualizaruser.telefono,
+        Visualizaruser.rol,
+      ]),
+    ];
+    const ws = XLSX.utils.aoa_to_sheet(wsData);
+
+    // Agrega estilos de tabla a la hoja de cálculo
+    ws["!cols"] = [
+      { width: 30 },
+      { width: 30 },
+      { width: 30 },
+      { width: 30 },
+      { width: 30 },
+      { width: 30 },
+    ];
+
+    // Genera el archivo Excel
+    XLSX.utils.book_append_sheet(wb, ws, "Usuario");
+    XLSX.writeFile(wb, "proyectos.xlsx");
+  };
+
     return (
         <div className="main-container__contenedor-hijo main-container__contenedor-hijo--color">
           <Header_ToolBar
@@ -23,6 +72,7 @@ function Visualizar_Usuario_Admin() {
                   icon={<FaFileArrowUp />}
                   text={"Reporte"}
                   clase={"btn-blanco btn-blanco--modify btn-verde"}
+                  onClick={exportToExcel}
                 />
                 <Search text={"Buscar usuarios"} />
                 <BotonVerdeAñadir
@@ -47,14 +97,28 @@ function Visualizar_Usuario_Admin() {
                   </tr>
                 </thead>
                 <tbody>
-                  <tr className="user-table__row">
-                    <td className="user-table__cell">Yesid</td>
-                    <td className="user-table__cell">Molona</td>
-                    <td className="user-table__cell">1059695065</td>
-                    <td className="user-table__cell">yesid@gmail.com</td>
-                    <td className="user-table__cell">3192819219</td>
-                    <td className="user-table__cell">admin</td>
-                  </tr>
+                  {Usuario.map ((Visualizaruser, index) => (
+                    <tr key={index} className="user-table__row">
+                      <td className="user-table__cell">
+                        {Visualizaruser.name}
+                      </td>
+                      <td className="user-table__cell">
+                        {Visualizaruser.last_names}
+                      </td>
+                      <td className="user-table__cell">
+                        {Visualizaruser.documento}
+                      </td>
+                      <td className="user-table__cell">
+                        {Visualizaruser.email}
+                      </td>
+                      <td className="user-table__cell">
+                      {Visualizaruser.telefono}
+                      </td>
+                      <td className="user-table__cell">
+                        {Visualizaruser.rol}
+                      </td>
+                    </tr>
+                  ))}
                 </tbody>
               </table>
             }
